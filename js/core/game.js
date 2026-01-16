cf73345dcdaf50a05671fe06b87ae3e49bf616d3
deraskenny <deraskennyson@gmail.com>
2026-01-16 07:37:41 +0100

    Update game.js

--- js/core/game.js
+++ js/core/game.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", () => {
+    console.log("GAME.JS LÄUFT"); // Testen, ob Script ausgeführt wird
+
     const canvas = document.getElementById("game");
-    canvas.width = Math.min(window.innerWidth*0.9,360);
-    canvas.height = canvas.width*16/9;
+    if(!canvas){
+        console.error("Canvas nicht gefunden!");
+        return;
+    }
+
     const ctx = canvas.getContext("2d");
-    ctx.fillStyle="red";
+    ctx.fillStyle = "red";
     ctx.fillRect(20,20,100,100);
-    console.log("Canvas Test OK");
 });
